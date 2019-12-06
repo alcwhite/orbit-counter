8e249635cce286bbb85c70f8d837112cbf4c0161
Al White <alcwhite13@gmail.com>
2019-12-06 16:25:20 -0500

    Set up orbit_counter namespace

---
+++ Orbits.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace orbit_counter
+{
+    public static class Orbits
+    {
+        public static int OrbitCount(string map)
+        {
+            return 0;
+        }
+    }
+}
