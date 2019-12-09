ebd621f24a45de46ff0be35e3ed166d0cc11c966
Al White <alcwhite13@gmail.com>
2019-12-09 10:45:02 -0500

    Fix looping error; change input to string

--- Orbits.cs
+++ Orbits.cs
@@ -5,22 +5,26 @@ namespace orbit_counter
 {
     public static class Orbits
     {
-        public static int OrbitCount(List<string> map)
+        public static int OrbitCount(string map)
         {
             int links = 0;
             var parsedMap = new Dictionary<string, string>();
-            map.ForEach(orbit => {
-                var splitOrbit = orbit.Split(')');
+            var splitMap = map.Split("\n");
+            foreach (string orbit in splitMap)
+            {
+                var splitOrbit = orbit.Split(')', 2);
                 parsedMap.Add(splitOrbit[1], splitOrbit[0]);
-            });
+            };
             foreach (KeyValuePair<string, string> entry in parsedMap) 
             {
+                Console.WriteLine(entry);
                 int keyLinks = 1;
                 string value = entry.Value;
-                while (value != "COM")
+                bool doAgain = true;
+                while (value != "COM" && doAgain)
                 {
                     keyLinks++;
-                    parsedMap.TryGetValue(entry.Value, out string newValue);
+                    doAgain = parsedMap.TryGetValue(value, out string newValue);
                     value = newValue;
                 }
                 links += keyLinks;
--- OrbitsTest.cs
+++ OrbitsTest.cs
@@ -3,22 +3,10 @@ using System.Collections.Generic;
 
 public class OrbitsTest
 {
+    string map = "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L";
     [Fact]
     public void Example_input()
     {
-        var map = new List<string>();
-        map.Add("COM)B");
-        map.Add("B)C");
-        map.Add("C)D");
-        map.Add("D)E");
-        map.Add("E)F");
-        map.Add("B)G");
-        map.Add("G)H");
-        map.Add("D)I");
-        map.Add("E)J");
-        map.Add("J)K");
-        map.Add("K)L");
-        
         Assert.Equal(42, orbit_counter.Orbits.OrbitCount(map));
     }
 }
